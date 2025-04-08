25cb06c394964629b03a0ec3821f4c4091b8565f
saidivyateja12 <saidivyateja6@gmail.com>
2025-04-08 23:46:49 +0530

    Implemented Symptom Checker screen with inputs for age, gender, symptoms, duration, pain level, and allergies. Added logic to suggest relevant first aid remedies based on user responses.

--- app/src/main/java/com/example/firstaidguide/FirstAidDashActivity.kt
+++ app/src/main/java/com/example/firstaidguide/FirstAidDashActivity.kt
@@ -1,9 +1,13 @@
 package com.example.firstaidguide
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -23,9 +28,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -33,7 +41,7 @@ class FirstAidDashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FirstAidDashScreen()
+            FirstAidDashboard()
         }
     }
 }
@@ -156,4 +164,158 @@ fun CardWithImageAndText(
             )
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FirstAidDashboardP() {
+    FirstAidDashboard()
+}
+
+@Composable
+fun FirstAidDashboard() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = colorResource(id = R.color.app_bar_color))
+                .padding(horizontal = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Image(
+                modifier = Modifier
+                    .size(36.dp),
+                painter = painterResource(id = R.drawable.first_aid),
+                contentDescription = "First Aid"
+            )
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Text(
+                modifier = Modifier
+                    .padding(12.dp),
+                text = "First Aid Guide",
+                style = MaterialTheme.typography.headlineSmall.copy(
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+
+        }
+        
+        Spacer(modifier = Modifier.height(12.dp))
+
+
+        FirstAidHomeItem("Search\nGuideline",R.drawable.first_aid,"Search guidelines for different symptoms","Search Guidelines")
+
+        FirstAidHomeItem("First Aid\nGuide",R.drawable.first_aid,"See the guidelines for various emergencies","View Guidelines")
+
+        FirstAidHomeItem("Emergency\nContacts",R.drawable.first_aid,"See the guidelines for various emergencies","View Contacts")
+
+        FirstAidHomeItem("Saved\nGuidelines",R.drawable.first_aid,"See the guidelines for various emergencies","View Bookamarks")
+
+        FirstAidHomeItem("My Medical\nInfo",R.drawable.first_aid,"See the guidelines for various emergencies","View Info")
+
+    }
+}
+
+@Composable
+fun FirstAidHomeItem(title: String,imageRes: Int,caption: String,buttonText: String)
+{
+    val context = LocalContext.current
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 12.dp, vertical = 8.dp)
+            .background(
+                color = colorResource(id = R.color.c1_bg),
+                shape = RoundedCornerShape(6.dp)
+            )
+            .border(
+                width = 1.dp,
+                color = colorResource(id = R.color.c1_bg),
+                shape = RoundedCornerShape(6.dp)
+            )
+            .padding(8.dp)
+    ) {
+
+        Spacer(modifier = Modifier.width(20.dp))
+
+        Column {
+
+            Text(
+                modifier = Modifier,
+                text = title,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge.copy(
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+            Image(
+                modifier = Modifier
+                    .size(64.dp),
+                painter = painterResource(id = imageRes),
+                contentDescription = "Guide"
+            )
+
+        }
+
+        Spacer(modifier = Modifier.width(40.dp))
+
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = caption,
+                style = MaterialTheme.typography.titleSmall.copy(
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+            Spacer(modifier = Modifier.height(14.dp))
+
+
+            Text(
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .clickable {
+                        when(imageRes)
+                        {
+                            R.drawable.first_aid -> {
+                                context.startActivity(Intent(context, SearchGuidelinesActivity::class.java))
+                            }
+                        }
+                    }
+                    .background(
+                        color = colorResource(id = R.color.white),
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .border(
+                        width = 1.dp,
+                        color = colorResource(id = R.color.white),
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .padding(horizontal = 12.dp, vertical = 4.dp),
+                text = buttonText,
+                textAlign = TextAlign.Center,
+                color = colorResource(id = R.color.c1_bg),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/firstaidguide/SearchGuidelinesActivity.kt
@@ -0,0 +1,287 @@
+package com.example.firstaidguide
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class SearchGuidelinesActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SearchGuidelinesScreen()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchGuidelinesScreenP() {
+    SearchGuidelinesScreen()
+}
+
+@Composable
+fun SearchGuidelinesScreen() {
+    var symptoms by remember { mutableStateOf("") }
+
+    var age by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
+    var duration by remember { mutableStateOf("") }
+    var painLevel by remember { mutableStateOf("") }
+    var allergies by remember { mutableStateOf("") }
+
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = colorResource(id = R.color.app_bar_color))
+                .padding(horizontal = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Image(
+                modifier = Modifier
+                    .size(36.dp),
+                painter = painterResource(id = R.drawable.back),
+                contentDescription = "Back"
+            )
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Text(
+                modifier = Modifier
+                    .padding(12.dp),
+                text = "Search Guidelines",
+                style = MaterialTheme.typography.headlineSmall.copy(
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+
+        }
+
+        Column(
+            modifier = Modifier.padding(horizontal = 12.dp)
+        ) {
+
+
+            BasicTextField(
+                value = symptoms,
+                onValueChange = { symptoms = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp)
+                    .height(50.dp),
+                singleLine = true,
+                decorationBox = { innerTextField ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .background(Color.LightGray, MaterialTheme.shapes.medium)
+                            .padding(horizontal = 16.dp)
+                            .fillMaxSize()
+                    ) {
+                        Box(modifier = Modifier.weight(1f)) {
+                            if (symptoms.isEmpty()) {
+                                Text(text = "Symptoms", color = Color.Gray)
+                            }
+                            innerTextField()
+                        }
+
+                    }
+                }
+            )
+
+            Row {
+                BasicTextField(
+                    value = symptoms,
+                    onValueChange = { symptoms = it },
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(vertical = 8.dp)
+                        .height(50.dp),
+                    singleLine = true,
+                    decorationBox = { innerTextField ->
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                                .background(Color.LightGray, MaterialTheme.shapes.medium)
+                                .padding(horizontal = 16.dp)
+                                .fillMaxSize()
+                        ) {
+                            Box(modifier = Modifier.weight(1f)) {
+                                if (symptoms.isEmpty()) {
+                                    Text(text = "Age", color = Color.Gray)
+                                }
+                                innerTextField()
+                            }
+
+                        }
+                    }
+                )
+
+                Spacer(modifier = Modifier.width(12.dp))
+
+                BasicTextField(
+                    value = symptoms,
+                    onValueChange = { symptoms = it },
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(vertical = 8.dp)
+                        .height(50.dp),
+                    singleLine = true,
+                    decorationBox = { innerTextField ->
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                                .background(Color.LightGray, MaterialTheme.shapes.medium)
+                                .padding(horizontal = 16.dp)
+                                .fillMaxSize()
+                        ) {
+                            Box(modifier = Modifier.weight(1f)) {
+                                if (symptoms.isEmpty()) {
+                                    Text(text = "Gender", color = Color.Gray)
+                                }
+                                innerTextField()
+                            }
+
+                        }
+                    }
+                )
+            }
+
+            BasicTextField(
+                value = symptoms,
+                onValueChange = { symptoms = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp)
+                    .height(50.dp),
+                singleLine = true,
+                decorationBox = { innerTextField ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .background(Color.LightGray, MaterialTheme.shapes.medium)
+                            .padding(horizontal = 16.dp)
+                            .fillMaxSize()
+                    ) {
+                        Box(modifier = Modifier.weight(1f)) {
+                            if (symptoms.isEmpty()) {
+                                Text(text = "Duration of symptoms", color = Color.Gray)
+                            }
+                            innerTextField()
+                        }
+
+                    }
+                }
+            )
+
+            BasicTextField(
+                value = symptoms,
+                onValueChange = { symptoms = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp)
+                    .height(50.dp),
+                singleLine = true,
+                decorationBox = { innerTextField ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .background(Color.LightGray, MaterialTheme.shapes.medium)
+                            .padding(horizontal = 16.dp)
+                            .fillMaxSize()
+                    ) {
+                        Box(modifier = Modifier.weight(1f)) {
+                            if (symptoms.isEmpty()) {
+                                Text(text = "Pain Level", color = Color.Gray)
+                            }
+                            innerTextField()
+                        }
+
+                    }
+                }
+            )
+
+            BasicTextField(
+                value = symptoms,
+                onValueChange = { symptoms = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp)
+                    .height(50.dp),
+                singleLine = true,
+                decorationBox = { innerTextField ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .background(Color.LightGray, MaterialTheme.shapes.medium)
+                            .padding(horizontal = 16.dp)
+                            .fillMaxSize()
+                    ) {
+                        Box(modifier = Modifier.weight(1f)) {
+                            if (symptoms.isEmpty()) {
+                                Text(text = "Allergies", color = Color.Gray)
+                            }
+                            innerTextField()
+                        }
+
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                shape = MaterialTheme.shapes.medium,
+                colors = ButtonDefaults.buttonColors(Color(0xFF5D3FD3))
+            ) {
+                Text(
+                    text = "Search", color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+        }
+    }
+}
