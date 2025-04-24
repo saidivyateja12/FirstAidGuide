6f3272af3fefcb723aa6a47dc750bcc269683e92
saidivyateja12 <saidivyateja6@gmail.com>
2025-04-24 23:55:01 +0530

    Added GPS-based nearby hospitals locator and About Us page with contact info

---
+++ app/src/main/java/com/example/firstaidguide/SyptomCheckerActivity.kt
@@ -0,0 +1,13 @@
+package com.example.firstaidguide
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SyptomCheckerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
